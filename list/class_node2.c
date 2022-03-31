df6fa132c23266d2963f3cf0ce6da2d778f3b137
cedric0708 <81212131+cedric0708@users.noreply.github.com>
2022-03-31 17:22:21 +0900

    Update class_node2.c

--- list/class_node2.c
+++ list/class_node2.c
@@ -38,6 +38,75 @@ void insertlast(Listtype* l, char c) {
 	}
 }
 
+void insert(Listtype* l, int pos, char c) {
+	Listnode* node = (Listnode*)malloc(sizeof(Listnode));
+	Listnode* p=l->h;
+
+	if (pos == 1)
+		insertfirst(l, c);
+	else {
+		for (int i = 1; i < pos - 1; i++)
+			p = p->next;
+		node->elem = c;
+		node->next = p->next;
+		p->next = node;
+	}
+}
+
+char deletefirst(Listtype* l) {
+	Listnode* p = l->h;
+	char c = p->elem;
+	l->h = p->next;
+	free(p);
+	return c;
+}
+
+char deletelast(Listtype* l) {
+	Listnode* p = l->h;
+	Listnode* q = p;
+
+	while (p->next != NULL) {
+		q = p;
+		p = p->next;
+	}
+	char c = p->elem;
+	q->next = NULL;
+	free(p);
+	return c;
+}
+
+char delete(Listtype* l, int pos) {
+	Listnode* p = l->h;
+	Listnode* q = p;
+
+	if (pos == 1)
+		return deletefirst(l);
+	else {
+		for (int i = 1; i <= pos-1; i++) {
+			q = p;
+			p = p->next;
+		}
+		char c = p->elem;
+		q->next = p->next;
+		free(p);
+		return c;
+	}
+}
+
+void partition(Listtype* l, Listtype* l1, Listtype* l2, int pos) {
+	Listnode* p = l->h;
+	l1->h = p;
+	for (int i = 1; i < pos; i++)
+		p = p->next;
+
+	l2->h = p->next;
+	p->next = NULL;
+}
+
+void concat(Listtype* l1, Listtype* l2) {
+
+}
+
 void print(Listtype* l) {
 	for (Listnode* p = l->h; p != NULL; p=p->next) {
 		printf("%c -> ", p->elem);
@@ -58,6 +127,27 @@ int main()
 	insertfirst(&l, 'A'); print(&l);
 	insertfirst(&l, 'B'); print(&l);
 	insertfirst(&l, 'C'); print(&l);
+	getchar();
+
+	insert(&l, 2, 'G'); print(&l);
+	insert(&l, 4, 'H'); print(&l);
+	insert(&l, 6, 'I'); print(&l);
+	getchar();
+
+	printf("%c\n", deletefirst(&l)); print(&l);
+	deletelast(&l); print(&l);
+	delete(&l, 3); print(&l);
+
+	Listtype l1, l2;
+	int pos;
+	printf("Partition position : ");
+	scanf("%d", &pos);
+
+	partition(&l, &l1, &l2, pos);
+	print(&l1); print(&l2);
+	getchar(); getchar();
+
+	concat(&l1, &l2); print(&l1);
 
 	return 0;
 }
