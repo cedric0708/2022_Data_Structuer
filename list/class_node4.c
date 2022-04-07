d9259f46319122facdbf4a4d9ae986e27cc16a5a
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-04-07 15:04:15 +0900

    Create class_node4.c

---
+++ list/class_node4.c
@@ -0,0 +1,106 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct dlistnode {
+	char data;
+	struct dlistnode* prev, * next;
+}dlistnode;
+
+typedef struct {
+	dlistnode* h;
+	dlistnode* t;
+}dlisttype;
+
+void init(dlisttype *dl) {
+	dl->h = dl->t = NULL;
+}
+
+void insertfirst(dlisttype* dl, char c) {
+	dlistnode* node = (dlistnode*)malloc(sizeof(dlistnode));
+	dlistnode* p = dl->h;
+	node->data = c;
+	node->prev = node->next = NULL;
+
+	if (p == NULL)
+		dl->h = dl->t = node;
+	else {
+		node->next = p;
+		p->prev = node;
+		dl->h = node;
+	}
+
+}
+
+void insertlast(dlisttype* dl, char c) {
+	dlistnode* node = (dlistnode*)malloc(sizeof(dlistnode));
+	dlistnode* p = dl->t;
+	node->data = c;
+	node->prev = node->next = NULL;
+
+	if (p == NULL)
+		dl->h = dl->t = node;
+	else {
+		node->prev = p;
+		p->next = node;
+		dl->t = node;
+	}
+
+}
+
+void insert(dlisttype* dl, int pos, char c) {
+	dlistnode* node = (dlistnode*)malloc(sizeof(dlistnode));
+	dlistnode* p = dl->h;
+
+	if (pos == 1)
+		insertfirst(dl, c);
+	else{
+		for (int i = 1; i < pos - 1; i++) {
+			p = p->next;
+		}
+		node->data = c;
+		node->prev = p;
+		node->next = p->next;
+
+		if (p->next == NULL) {
+			p->next = node;
+			dl->t = node;
+		}
+		else {
+			p->next->prev = node;
+		}
+	}
+}
+
+void printforward(dlisttype* dl) {
+	for (dlistnode* p = dl->h; p != NULL; p = p->next)
+		printf("[%c] <=> ", p->data);
+	printf("\b\b\b\b   \n");
+}
+
+void printbackward(dlisttype* dl) {
+	for (dlistnode* p = dl->t; p != NULL; p = p->prev)
+		printf("[%c] <=> ", p->data);
+	printf("\b\b\b\b   \n");
+}
+
+int main()
+{
+	dlisttype dl;
+	init(&dl);
+
+	insertfirst(&dl, 'A'); insertfirst(&dl, 'B');
+	printforward(&dl); printbackward(&dl);
+
+	insertlast(&dl, 'C'); insertlast(&dl, 'D');
+	printforward(&dl); printbackward(&dl);
+	
+	insert(&dl, 1, 'E'); insert(&dl, 3, 'F');
+	printforward(&dl); printbackward(&dl);
+	
+
+	
+
+	return 0;
+}
