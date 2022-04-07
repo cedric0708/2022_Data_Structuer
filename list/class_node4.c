1bf378aab5f8be65e98190356ec76716f2682721
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-04-07 15:21:11 +0900

    Update class_node4.c

--- list/class_node4.c
+++ list/class_node4.c
@@ -13,7 +13,7 @@ typedef struct {
 	dlistnode* t;
 }dlisttype;
 
-void init(dlisttype *dl) {
+void init(dlisttype* dl) {
 	dl->h = dl->t = NULL;
 }
 
@@ -55,7 +55,7 @@ void insert(dlisttype* dl, int pos, char c) {
 
 	if (pos == 1)
 		insertfirst(dl, c);
-	else{
+	else {
 		for (int i = 1; i < pos - 1; i++) {
 			p = p->next;
 		}
@@ -71,6 +71,56 @@ void insert(dlisttype* dl, int pos, char c) {
 			p->next->prev = node;
 		}
 	}
+}
+
+char deletefirst(dlisttype* dl) {
+	dlistnode* p = dl->h;
+	char c = p->data;
+
+	if (p->next == NULL)
+		dl->h = dl->t = NULL;
+	else {
+		p->next->prev = NULL;
+		dl->h = p->next;
+	}
+	free(p);
+	return c;
+}
+
+char deletelast(dlisttype* dl) {
+	dlistnode* p = dl->h;
+	char c = p->data;
+
+	if (p->next == NULL)
+		dl->h = dl->t = NULL;
+	else {
+		p->prev->next= NULL;
+		dl->t = p->prev;
+	}
+	free(p);
+	return c;
+}
+
+char delete(dlisttype* dl, int pos) {
+	dlistnode* p = dl->h;
+	char c;
+
+	if (pos == 1)
+		c = deletefirst(dl);
+	else {
+		for (int i = 1; i < pos; i++)
+			p = p->next;
+
+		if (p->next == NULL)
+			c = deletelast(dl);
+		else {
+			c = p->data;
+			p->prev->next = p->next;
+			p->next->prev = p->prev;
+			free(p);
+		}
+	}
+	return c;
 }
 
 void printforward(dlisttype* dl) {
@@ -95,12 +145,15 @@ int main()
 
 	insertlast(&dl, 'C'); insertlast(&dl, 'D');
 	printforward(&dl); printbackward(&dl);
-	
+
 	insert(&dl, 1, 'E'); insert(&dl, 3, 'F');
 	printforward(&dl); printbackward(&dl);
-	
+	getchar();
 
-	
+	deletefirst(&dl); printforward(&dl);
+	delete(&dl, 3); printforward(&dl);
+
+
 
 	return 0;
 }
