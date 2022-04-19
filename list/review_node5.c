ddefbe7d7d5fe8267c709dcc3f33d55f7ac8f174
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-04-19 23:13:14 +0900

    Update review_node5.c

--- list/review_node5.c
+++ list/review_node5.c
@@ -9,7 +9,7 @@
 typedef struct listnode {
 	int elem;
 	struct listnode* prev, * next;
-}listnode;
+}listnode;//이중연결리스트
 
 void init(listnode* H[], listnode* T[]) {
 	for (int i = 0; i < size; i++) {
@@ -20,7 +20,7 @@ void init(listnode* H[], listnode* T[]) {
 		H[i] = h;
 		T[i] = t;
 	}
-}
+}//초기화
 
 void add(listnode* H[], int g, int e) {
 	listnode* node = (listnode*)malloc(sizeof(listnode));
@@ -29,30 +29,31 @@ void add(listnode* H[], int g, int e) {
 	node->next = H[g]->next;
 	H[g]->next->prev = node;
 	H[g]->next = node;
-}
+}//노드 추가
 
 void traverse(listnode* H[], listnode* T[]) {
-	listnode* p;
 	for (int i = 0; i < size; i++) {
-		for (p = H[i]->next; p != T[i]; p = p->next)
+		listnode* p = H[i]->next;
+		for (; p != T[i]; p = p->next)
 			printf(" %d", p->elem);
 		printf("\n");
 	}
-}
+}//출력
 
 
 
 int main()
 {
-	listnode* h[size],* t[size];
+	listnode* h[size], * t[size];
 	int g, e;
 
 	init(h, t);
+	srand(time(NULL));
 
-	for (int i = 1; i <= 6; i++) {
-		scanf("%d %d", &g, &e);
+	for (int i = 1; i <= 10; i++) {
+		g = rand() % 3;
+		e = rand() % 100;
 		add(h, g, e);
-		getchar();
 	}
 
 	traverse(h, t);
