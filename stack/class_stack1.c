e7e50509c842cca09c9e028213b7f30aee79adec
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-04-28 14:23:03 +0900

    Create class_stack1.c

---
+++ stack/class_stack1.c
@@ -0,0 +1,68 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define n 100
+
+typedef struct {
+	char data[n];
+	int top;
+}stacktype;
+
+void init(stacktype* s) {
+	s->top = -1;
+}
+
+int isempty(stacktype* s) {
+	return s->top == -1;
+}
+
+int isfull(stacktype* s) {
+	return s->top == n - 1;
+}
+
+void push(stacktype* s ,char e) {
+	if (isfull(s))
+		printf("Stack Full\n");
+	else {
+		s->top++;
+		s->data[s->top] = e;
+	}
+}
+
+char pop(stacktype* s) {
+	if (isempty(s)) {
+		printf("Stack Empty\n");
+		return -1;
+	}
+
+	char e = s->data[s->top];
+	s->top--;
+	return e;
+}
+
+char peek(stacktype* s) {
+	if (isempty(s)) {
+		printf("Stack Empty\n");
+		return -1;
+	}
+
+	return s->data[s->top];
+}
+
+void print(stacktype* s) {
+	for (int i = s->top; i >= 0; i--)
+		printf("%c", s->data[i]);
+	printf("\n");
+}
+
+int main()
+{
+	stacktype* s;
+	init(&s);
+
+	pop(&s);
+
+	return 0;
+}
